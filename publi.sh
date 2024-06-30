a880a6bbbbe0466083768da86e85540c6905b6cc
WHYjun <yjunchoi93@gmail.com>
2024-06-30 15:47:08 -0500

    Add comments to automation script

--- publi.sh
+++ publi.sh
@@ -1,3 +1,11 @@
+# This script was copied from https://github.com/ByeongsuPark/byeongsupark.github.io
+# Date copied: 2024-06-30
+# Original Author: Byeongsu Park
+#
+# Modifications made by: Young Choi
+# Date modified: 2024-06-30
+# Description of modifications: Updated branch names.
+
 git checkout main
 rm -rf _site/
 JEKYLL_ENV=production bundle exec jekyll build 
